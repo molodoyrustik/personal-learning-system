8868abeffad49d1fcd34928cdbc5adb36f411355
Ruslan Bagautdinov <ruslan.bagautdinov@tabby.ai>
2026-06-21 22:11:16 +0300

    fix sign up

--- src/app/sign-up/actions.ts
+++ src/app/sign-up/actions.ts
@@ -2,13 +2,18 @@
 
 import { redirect } from "next/navigation";
 import { createClient } from "@/shared/lib/supabase/server";
+import { headers } from "next/headers";
 
 export async function signUp(formData: FormData) {
   const supabase = await createClient();
+  const origin = (await headers()).get("origin");
 
   const { data, error } = await supabase.auth.signUp({
     email: formData.get("email") as string,
     password: formData.get("password") as string,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
   });
 
   if (error) {
